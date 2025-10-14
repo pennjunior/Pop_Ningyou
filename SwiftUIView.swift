bedbdc2a91110184041d18e5b1f967163c8f3184
pennjunior <pennchojunior@gmail.com>
2025-10-14 12:16:49 +0900

    terminal connection tested

---
+++ SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  Pop_Ningyou
+//
+//  Created by cmAirS043 on 2025/10/14.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
